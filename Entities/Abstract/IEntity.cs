52633c59449216976f864106ee571bc1b85bc861
eren kut <eren.kut.15@gmail.com>
2021-02-10 15:03:05 +0300

    Proje dosyası ekle.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir veri tabanı tablosu dur
+    public interface IEntity
+    {
+
+    }
+}
